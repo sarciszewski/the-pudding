0862430393eb71f16a5e0f87d1b895beb490c45a
kobra <kobra@voodoo>
2013-12-14 16:54:48 -0500

    * Pushing changes before I go AFK

--- public/index.php
+++ public/index.php
@@ -1,11 +1,1 @@
 <?php
-header("Content-Type: text/plain;charset=UTF-8");
-include_once "../libraries/scrypt.php";
-
-$p = Password::hash('dsfargeg');
-echo "{$p}\n";
-echo "Length: ".strlen($p)."\n";
-
-if(Password::check('dsfargeg', $p)) {
-  echo 'scrypt works!';
-}
