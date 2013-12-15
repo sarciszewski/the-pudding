8dd2a472cf671583cc3560943f5f09f8edb33568
kobra <kobra@voodoo>
2013-12-14 20:30:01 -0500

    * Session regeneration after N seconds

--- session.php
+++ session.php
@@ -33,7 +33,24 @@ if($newSession) {
   } else {
     // Let's verify our canary values!
     if($settings['session']['bind_to_ip']) {
-      
-    }
-  }
-}
+      // If the hashed IP doesn't match our records, log everyone out.
+      if(!slow_equals(
+        $_SESSION['canary']['ip'],
+        hash_hmac('sha256', $_SERVER['REMOTE_ADDR'], file_get_contents('/tmp/ip_hash_key.key')))
+      ) {
+        foreach(array_keys($_SESSION) as $i) {
+          unset($_SESSION[$i]); // NO U
+        }
+        session_regenerate_id(true);
+        header("Location: /"); exit;
+      } // end if(!slow_equals())
+    } // <- end if($settings['session']['bind_to_ip'])
+    if($settings['session']['regen_time'] > 0) {
+      // Let's rotate our session ID every ___ seconds
+      if(time() - $_SESSION['canary']['birth'] >= $settings['session']['regen_time']) {
+        $_SESSION['canary']['birth'] = time();
+        session_regenerate_id(true);
+      }
+    } // <- end if($settings['session']['regen_time'])
+  } // <- end else for if(empty($_SESSION['canary']))
+} // end else for if($newSession)
