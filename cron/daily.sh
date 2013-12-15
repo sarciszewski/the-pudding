31d2aaa8e6b1f73b087ed48b30b18fa7d6ffef94
kobra <kobra@voodoo>
2013-12-14 20:37:06 -0500

    * Reorganized IP hashing, added shred to daily cronjob script

--- cron/daily.sh
+++ cron/daily.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Run this every day
+shred /tmp/ip_hash_key.key
 php randomString.php 63 > /tmp/ip_hash_key.key
--- session.php
+++ session.php
@@ -2,6 +2,26 @@
 if($_SERVER['SERVER_PORT'] === 443) {
   ini_set('session.cookie_secure', true); // Just in case HSTS fails somehow
 }
+// Inspired by cryptolog; don't store real IP addresses anywhere if possible!
+if(!file_exists('/tmp/ip_hash_key.key')) {
+  file_put_contents('/tmp/ip_hash_key.key',
+    substr(base64_encode(openssl_random_pseudo_bytes( 48 )), 0, 63)
+  );
+  if(!file_exists('/tmp/ip_hash_key.key')) {
+    die("Website offline. Please make daily IP key writeable.");
+  }
+} elseif(time() - filemtime('/tmp/ip_hash_key.key') > 86400) {
+  // This shit needs to be rotated daily
+  file_put_contents('/tmp/ip_hash_key.key',
+    substr(base64_encode(openssl_random_pseudo_bytes( 48 )), 0, 63)
+  );
+  if(time() - filemtime('/tmp/ip_hash_key.key') > 86400) {
+    die("Website offline. Please make daily IP key overwriteable.");
+  }
+}
+// Use this instead
+$ip = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'],
+        file_get_contents('/tmp/ip_hash_key.key'));
 $newSession = false;
 if(isset($_COOKIE[ini_get('session.name')])) {
   $newSession = true;
@@ -12,14 +32,7 @@ if($newSession) {
     'birth' => time()
   ];
   if($settings['session']['bind_to_ip']) {
-    // Cryptolog
-    if(!file_exists('/tmp/ip_hash_key.key')) {
-      file_put_contents('/tmp/ip_hash_key.key',
-        substr(base64_encode(openssl_random_pseudo_bytes( 48 )), 0, 63)
-      );
-    }
-    $_SESSION['canary']['ip'] = hash_hmac('sha256', $_SERVER['REMOTE_ADDR'], 
-      file_get_contents('/tmp/ip_hash_key.key'));
+    $_SESSION['canary']['ip'] = $ip;
     // TODO: Use shm to keep the key in memory rather than on disk
   }
 } else {
@@ -34,16 +47,14 @@ if($newSession) {
     // Let's verify our canary values!
     if($settings['session']['bind_to_ip']) {
       // If the hashed IP doesn't match our records, log everyone out.
-      if(!slow_equals(
-        $_SESSION['canary']['ip'],
-        hash_hmac('sha256', $_SERVER['REMOTE_ADDR'], file_get_contents('/tmp/ip_hash_key.key')))
-      ) {
+      if(!slow_equals($_SESSION['canary']['ip'], $ip)) {
+        // You are the weakest link. Good-bye!
         foreach(array_keys($_SESSION) as $i) {
           unset($_SESSION[$i]); // NO U
         }
         session_regenerate_id(true);
         header("Location: /"); exit;
-      } // end if(!slow_equals())
+      } // end if(!slow_equals($_SESSION['canary']['ip'], $ip))
     } // <- end if($settings['session']['bind_to_ip'])
     if($settings['session']['regen_time'] > 0) {
       // Let's rotate our session ID every ___ seconds
